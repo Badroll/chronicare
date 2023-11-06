47444bf5cb4b6a82274cbf14edb4ad7a340fbfde
Badrull <badrl.xrpl2017smkrus@gmail.com>
2023-11-07 00:28:58 +0700

    +

--- env.py
+++ env.py
@@ -1,10 +1,9 @@
 development = "local"
+runHost = "0.0.0.0"
 if development == "local":
-    runHost = "192.168.0.146"
     fullPath = 'C:\\python\\chronicare'
     runDebug = True
 elif development == "doscom":
-    runHost = "172.16.0.41"
-    fullPath = '/home/flbadrul/chronicare'
+    fullPath = '/root/python/chronicare'
     runDebug = False
-runPort = 5000
+runPort = 5001
